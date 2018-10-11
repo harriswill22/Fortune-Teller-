4fd8aa422e7c1d0dfcbe13a57203a3382b9ceddd
Will Harris <Harriswill22@yahoo.com>
2018-10-11 10:32:37 -0400

    adds output element to display text

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,27 @@ console.log('im here')
 // '"Data attributes" are for JS"
 
 //  #1 Make some code run  when i click the button 
+// addEventListerner accepts 2 arguments:
+//  - a string that identifies the kind of event to listen for 
+// - a function that says what to do when the event happens 
+
 
 const triggerElement = document.querySelector('[data-trigger]');
+const outputElement = document.querySelector('[data-output]');
 
+triggerElement.addEventListener('click', function hello() {
+    console.log('Hello addEvent Listener!');
+    outputElement.textContent = " lalalalla"
+});
 
 
 // #2 Write some text to the page on the button click
+ 
+
+
+
+
+
 
 
 
