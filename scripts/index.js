14a511f0f057700cba4cb57d46ce2d9d7543de52
Will Harris <Harriswill22@yahoo.com>
2018-10-11 09:37:02 -0400

    adds comments and data attributes

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+console.log('im here')
+//  Classes are for styling 
+// '"Data attributes" are for JS"
+
+//  #1 Make some code run  when i click the button 
+
+const triggerElement = document.querySelector();
+
+
+
+// #2 Write some text to the page on the button click
+
+
+
+
+// #3 Pull  text from an array of text ti write to the page 
+
+
